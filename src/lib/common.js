517b2f45322581b8cc06f78dc2e6da3522ad948b
Willin Wang <willin@willin.org>
2022-09-22 19:53:46 +0800

    fix: fix null

--- src/lib/common.js
+++ src/lib/common.js
@@ -9,26 +9,33 @@ exports.formatDate = (inputPattern, inputDate) => {
     m: date.getMinutes(), // minute
     s: date.getSeconds() // second
   };
-  pattern = pattern.replace(/(y+)/ig, (a, b) => y.substr(4 - Math.min(4, b.length)));
+  pattern = pattern.replace(/(y+)/gi, (a, b) => y.substr(4 - Math.min(4, b.length)));
   /* eslint no-restricted-syntax:0,guard-for-in:0 */
   for (const i in o) {
-    pattern = pattern.replace(new RegExp(`(${i}+)`, 'g'), (a, b) => ((o[i] < 10 && b.length > 1) ? `0${o[i]}` : o[i]));
+    pattern = pattern.replace(new RegExp(`(${i}+)`, 'g'), (a, b) => (o[i] < 10 && b.length > 1 ? `0${o[i]}` : o[i]));
   }
   return pattern;
 };
 
-exports.nodeMap = (list, prev) => list.map((x, index) => {
-  const { followers: { totalCount: followers }, following: { totalCount: following },
-    repositories: { totalCount: repositories }, organizations: { totalCount: organizations },
-    ...info } = x;
-  const lastIndex = prev.findIndex(node => node.login === info.login);
-  return {
-    rank: index + 1,
-    change: ~lastIndex ? index - lastIndex : (1000 - index),
-    ...info,
-    followers,
-    following,
-    repositories,
-    organizations
-  };
-});
+exports.nodeMap = (list, prev) =>
+  list
+    .filter((x) => x !== null)
+    .map((x, index) => {
+      const {
+        followers: { totalCount: followers },
+        following: { totalCount: following },
+        repositories: { totalCount: repositories },
+        organizations: { totalCount: organizations },
+        ...info
+      } = x;
+      const lastIndex = prev.findIndex((node) => node.login === info.login);
+      return {
+        rank: index + 1,
+        change: ~lastIndex ? index - lastIndex : 1000 - index,
+        ...info,
+        followers,
+        following,
+        repositories,
+        organizations
+      };
+    });

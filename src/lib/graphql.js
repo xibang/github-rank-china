2a2be8b3350079b81fc3a653c6861235e43af7f0
Willin Wang <i@v0.md>
2024-10-26 16:28:41 +0800

    fix: add retry

--- src/lib/graphql.js
+++ src/lib/graphql.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const axiosRetry = require("axios-retry").default;
 
 axios.defaults.baseURL = "https://api.github.com";
 axios.defaults.headers.common.Authorization = `bearer ${
@@ -6,6 +7,8 @@ axios.defaults.headers.common.Authorization = `bearer ${
 }`;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.timeout = 10000;
+
+axiosRetry(axios, { retries: 3 });
 
 module.exports = ({ query, variables = undefined } = {}) =>
 	axios.post("/graphql", { query, variables }).then((x) => x.data);

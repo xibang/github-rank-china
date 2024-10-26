a6207eb99ef5e80f830fcaa94c55abf38e0ab379
Willin Wang <i@v0.md>
2024-10-26 16:31:15 +0800

    feat: sleep

--- src/rank/all.js
+++ src/rank/all.js
@@ -1,25 +1,29 @@
-const graphql = require('../lib/graphql');
-const { all: query } = require('../lib/gql');
-const { formatDate } = require('../lib/common');
+const graphql = require("../lib/graphql");
+const { all: query } = require("../lib/gql");
+const { formatDate, sleep } = require("../lib/common");
 
 module.exports = async () => {
-  let total = 0;
-  let list = [];
-  let after = '';
-  for (let i = 0; i < 10; i += 1) {
-    // eslint-disable-next-line no-console
-    console.log(`Processing no.${i + 1} request.`);
-    // eslint-disable-next-line no-await-in-loop
-    const result = await graphql({ query, variables: (i === 0 ? {} : { after }) });
-    after = result.data.search.pageInfo.endCursor;
+	let total = 0;
+	let list = [];
+	let after = "";
+	for (let i = 0; i < 10; i += 1) {
+		await sleep(200);
+		// eslint-disable-next-line no-console
+		console.log(`Processing no.${i + 1} request.`);
+		// eslint-disable-next-line no-await-in-loop
+		const result = await graphql({
+			query,
+			variables: i === 0 ? {} : { after },
+		});
+		after = result.data.search.pageInfo.endCursor;
 
-    const { userCount, nodes } = result.data.search;
-    total = userCount;
-    list = list.concat(nodes);
-  }
-  return {
-    total,
-    date: formatDate('yyyy-MM-dd'),
-    list
-  };
+		const { userCount, nodes } = result.data.search;
+		total = userCount;
+		list = list.concat(nodes);
+	}
+	return {
+		total,
+		date: formatDate("yyyy-MM-dd"),
+		list,
+	};
 };

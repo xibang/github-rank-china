10c27ace80d5c5f3bdee1385a66a7a1d49951a47
Willin Wang <i@v0.md>
2024-11-10 13:08:44 +0800

    update data

--- src/rank/all.js
+++ src/rank/all.js
@@ -2,12 +2,14 @@ const graphql = require("../lib/graphql");
 const { all: query } = require("../lib/gql");
 const { formatDate, sleep } = require("../lib/common");
 
+const timeout = process.env.CI ? 2048 : 1;
+
 module.exports = async () => {
 	let total = 0;
 	let list = [];
 	let after = "";
 	for (let i = 0; i < 10; i += 1) {
-		await sleep(2048);
+		await sleep(timeout);
 		// eslint-disable-next-line no-console
 		console.log(`Processing no.${i + 1} request.`);
 		// eslint-disable-next-line no-await-in-loop
